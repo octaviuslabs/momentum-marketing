9acb13a4d60aac2a21892d2f27e530ab949d8bcf
Jimi Smoot <jsfour@gmail.com>
2015-01-23 18:30:59 -0800

    first commit

---
+++ source/javascripts/all.js
@@ -0,0 +1,22 @@
+//= require "jquery-2.1.1.min"
+//= require "bootstrap.min"
+//= require "modernizr.custom"
+//= require "jquery.easing"
+//= require "retina"
+//= require "jquery.stellar.min"
+// %script{:defer => "defer", :src => "jquery.flexslider" >>>>>>> keeping this in here
+//= require "jquery.flexslider"
+//= require "jquery.parallax-1.1.3"
+//= require"count-to"
+//= require "jquery.appear"
+// %script{:defer => "defer", :src => "count-to"} >>>>>>> keeping this in here
+// %script{:defer => "defer", :src => "jquery.appear"} >>>>>>> keeping this in here
+//= require "jquery.mixitup"
+//= require "jquery.prettyPhoto"
+//= require "owl.carousel"
+//= require "jquery.easypiechart.min"
+//= require "jquery.validate.min"
+// %script{:defer => "defer", :src => "jquery.validate.min" >>>>>>> keeping this in here
+//= require "waypoints.min"
+//= require "smooth-scroll.min"
+//= require "custom"
