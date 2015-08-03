2bc229f97afe06e5697c7b08061858bd1a01e30e
Jimi Smoot <jsfour@gmail.com>
2015-08-03 16:16:07 -0700

    changed URL

--- config.rb
+++ config.rb
@@ -53,7 +53,7 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-set :url_root, 'http://www.meetvesper.com'
+set :url_root, 'http://www.vesper.ai'
 activate :search_engine_sitemap
 
 # Build-specific configuration
